670d87cb7fee25cbf9df71d23c244cab2572e5b1
Wen Wong <jwenwong@live.com>
2022-02-02 00:10:29 +1100

    add test cases for translated description endpoint

--- src/tests/Tests/PokemonControllerTests.cs
+++ src/tests/Tests/PokemonControllerTests.cs
@@ -43,5 +43,44 @@ namespace UnitTests
             var result = await _client.GetAsync($"/pokemon/{name}");
             result.IsSuccessStatusCode.Should().Be(false);
         }
+
+        [Fact]
+        public async Task GetTranslatedPokemon_WhenPokemonIsLegendary_ShouldReturnYodaTranslation()
+        {
+            var name = "mewtwo";
+            var result = await _client.GetAsync($"/pokemon/translated/{name}");
+            result.EnsureSuccessStatusCode();
+            var content = await result.Content.ReadAsAsync<PokemonResponse>();
+            content.Name.Should().Be(name);
+            content.Habitat.Should().Be("rare");
+            content.IsLegendary.Should().Be(true);
+            content.Description.Should().Be("Created by a scientist after years of horrific gene splicing and dna engineering experiments,  it was.");
+        }
+
+        [Fact]
+        public async Task GetTranslatedPokemon_WhenHabitatIsCave_ShouldReturnYodaTranslation()
+        {
+            var name = "zubat";
+            var result = await _client.GetAsync($"/pokemon/translated/{name}");
+            result.EnsureSuccessStatusCode();
+            var content = await result.Content.ReadAsAsync<PokemonResponse>();
+            content.Name.Should().Be(name);
+            content.Habitat.Should().Be("cave");
+            content.IsLegendary.Should().Be(false);
+            content.Description.Should().Be("Forms colonies in perpetually dark places.Ultrasonic waves to identify and approach targets,  uses.");
+        }
+
+        [Fact]
+        public async Task GetTranslatedPokemon_WhenNotLegendaryOrCave_ShouldReturnShakespeareTranslation()
+        {
+            var name = "charmander";
+            var result = await _client.GetAsync($"/pokemon/translated/{name}");
+            result.EnsureSuccessStatusCode();
+            var content = await result.Content.ReadAsAsync<PokemonResponse>();
+            content.Name.Should().Be(name);
+            content.Habitat.Should().Be("mountain");
+            content.IsLegendary.Should().Be(false);
+            content.Description.Should().Be("Obviously prefers hot places. At which hour 't rains,  steam is did doth sayeth to spout from the tip of its tail.");
+        }
     }
 }
